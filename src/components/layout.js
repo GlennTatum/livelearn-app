2c5f3e5d981185acdb43f0e344b71549fa480e60
matthewmAYEE <matthewmay852@gmail.com>
2024-07-22 14:16:25 -0400

    minor fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,7 @@
-import { useAuth } from "../AuthContext";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import NavBar from "./navBar";
 
 const Layout = () => {
-  const navigate = useNavigate();
-  const { isSignedIn, setIsSignedIn, setUserType } = useAuth();
-
-  const handleLogout = () => {
-    setIsSignedIn(false);
-    setUserType("");
-    navigate("/Login");
-  };
   return (
     <>
       <NavBar />
