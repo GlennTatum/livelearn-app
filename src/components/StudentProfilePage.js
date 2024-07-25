ffd03433d370991a287b6b4b2e877dd3f7ef6b90
GlennTatum <glennetatum@gmail.com>
2024-07-25 16:14:08 -0400

    initial student profile page and route

--- src/components/StudentProfilePage.js
+++ src/components/StudentProfilePage.js
@@ -1,7 +1,12 @@
+import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 export default function StudentProfilePage() {
     return (
-        <Image src="https://i.imgur.com/Y1EMauD.png" roundedCircle/>
+        <>
+            <Image src="https://i.imgur.com/Y1EMauD.png" roundedCircle/>
+            <Container></Container>
+        </>
+
     )
 }
